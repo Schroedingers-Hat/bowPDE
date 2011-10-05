0ffeabd26cc0d3e017631ae9b0cfc74849d4846f
Alex <turks.jpg@gmail.com>
2011-10-05 17:08:22 +1000

    free ends and such

--- setBoundaries.m
+++ setBoundaries.m
@@ -3,15 +3,15 @@
 % Vec((xPts+1)/2) = 0;
 % Vec((3*xPts+1)/2) = 0;
 % 
-% bw = 4;
-% 
-% for k = 1:bw
-%     Vec(k) = Vec(bw+1);
-%     Vec(xPts + 1 - k) = Vec(xPts - bw);
-%     
-%     Vec(xPts + bw + 1 - k) = Vec(xPts + bw + 2 - k) + Vec(xPts + bw + 1) - Vec(xPts + bw + 2);
-%     Vec(end - bw + k) = Vec(end - bw - 1 + k) + Vec(end - bw) - Vec(end - bw - 1);
-% end
+bw = 4;
+
+for k = 1:bw
+    Vec(k) = Vec(bw+1);
+    Vec(xPts + 1 - k) = Vec(xPts - bw);
+    
+    Vec(xPts + bw + 1 - k) = Vec(xPts + bw + 2 - k) + Vec(xPts + bw + 1) - Vec(xPts + bw + 2);
+    Vec(end - bw + k) = Vec(end - bw - 1 + k) + Vec(end - bw) - Vec(end - bw - 1);
+end
 % 
 
 outVec = Vec;
